a8527f7cc909e67f01c50945e6c6a0ae1c2414d2
Gabriel Pontes <gabriel.pontes@suporterenova.onmicrosoft.com>
2022-12-21 17:23:40 -0300

    Apoie

--- app/src/Views/Apoie/index.js
+++ app/src/Views/Apoie/index.js
@@ -3,6 +3,9 @@ import  React   from 'react';
 
 import NavbarInicial from '../../Components/Public/NavbarInicial';
 import Footer from '../../Components/Public/Footer';
+import ApoieLogo from '../../Assets/Img/caixaregistro.png'
+import  QrCode from '../../Assets/Img/qrcode.png'
+
 
 import './styles.css';
 
@@ -10,6 +13,14 @@ const Apoie = () => {
     return(
         <div className="Apoie">
             <NavbarInicial />
+            <div className='Container'>
+                <div className='QrCodeContent'>
+                    <img src={ApoieLogo} className='StockImage'/>
+                </div>
+                <div className='QrCodeContent'>
+                    <img src={QrCode} className='StockImage'/>
+                </div>
+            </div>
             <Footer />
         </div>
     );
