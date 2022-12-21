1a7ec0b12570559a661cb6002c02416c45ba9e11
Gabriel Pontes <gabriel.pontes@suporterenova.onmicrosoft.com>
2022-12-21 14:38:03 -0300

    Criação da Sidebar em Home

--- app/src/Views/Home/index.js
+++ app/src/Views/Home/index.js
@@ -1,12 +1,12 @@
 import  React   from 'react';
 
-
+import SideBar from '../../Components/Public/SideBar';
 import './styles.css';
 
 const Home = () => {
     return(
         <div className="Home">
-            
+            <SideBar />
         </div>
     );
 }
